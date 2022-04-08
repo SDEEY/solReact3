b429732f2419c86561b82ef679437bc9db7fd1f0
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-04-08 18:54:57 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.05
-const image = 'https://pbs.twimg.com/profile_images/1507151972071313409/q5yGVIS3_400x400.jpg' 
-const Title = 'Lovable Mullo NFT'
-const supply = 10000
+const solAmount = 0.09
+const image = 'https://pbs.twimg.com/profile_images/1512018229664772096/YcG_REdq_400x400.jpg' 
+const Title = 'Kyokudo Genesis'
+const supply = 4000
 
 document.title = Title
 
