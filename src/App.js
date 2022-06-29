59d92355acb8e5fc5e73abecef4f2008564eed84
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-06-29 23:52:33 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 1
-const image = 'https://howrare.is/drop_logos/5394_ey3VLUZq_400x400.jpg' 
-const Title = 'Mecrury SolTeeth DAO pass'
-const supply = 500
+const solAmount = 2.5
+const image = 'https://howrare.is/drop_logos/5220_A-UsWwo2.jpg' 
+const Title = 'PsyKey'
+const supply = 666
 
 document.title = Title
 
