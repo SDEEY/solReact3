1adf0b00f3cc51286d5b49a2d9d38980bfcc39bd
SDEEY <75689630+SDEEY@users.noreply.github.com>
2022-08-24 21:57:11 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import * as solanaWeb3 from "@solana/web3.js";
 import imgDiscord from './icons8-discord-50.png'
 import imgTwitter from './icons8-twitter-50.png'
 
-const solAmount = 0.8
-const image = 'https://pbs.twimg.com/profile_images/1552333289125453825/dcBJn-Pc_400x400.jpg' 
-const Title = 'Tokyo Saints'
-const supply = 4000
+const solAmount = 0.069
+const image = 'https://nftsolana.io/wp-content/uploads/2022/08/WF4XA-4w_400x400.jpeg' 
+const Title = 'Monsters Invasion'
+const supply = 1000
 
 document.title = Title
 
